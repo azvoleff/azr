688e8327f2ce81fe7b068d58019744335051156d
Alex Zvoleff <azvoleff@gmail.com>
2013-11-07 12:48:33 -0500

    Allow labeled timers.

--- R/trackTime.R
+++ R/trackTime.R
@@ -1,20 +1,30 @@
 #' Print status messages tracking run time for an R script
 #'
 #' @export
+#' @import lubridate
+#' @param an optional label used to maintain multiple tracking timers
 #' @param action a string indicating whether to \code{start} the timer or 
 #' \code{print} (default) the time that has passed since the timer was last 
 #' started.
 #' @examples
 #' #TODO: Add examples
-trackTime <- function(action='print') {
+trackTime <- function(label='', action='print') {
     if (action == 'start') {
-        assign('trackTime_timing', proc.time(), inherits=TRUE)
-        print('Reset tracking timer')
+        assign(paste('trackTime_starttime', label, sep='_'), now(), 
+               inherits=TRUE)
+        message('trackTime: Tracking timer reset.')
     } else if (action == 'print') {
-        if (exists("trackTime_timing")) {
-            print(proc.time() - trackTime_timing)
+        startTimeVar <- mget(paste('trackTime_starttime', label, sep='_'), 
+                             ifnotfound=list(NULL), inherits=TRUE)
+        if (!is.null(startTimeVar[[1]])) {
+            if (label != '') {
+                message(paste0('trackTime (', label, '): ', round(as.duration(now() - startTimeVar[[1]]), 2)))
+            } else {
+                message(paste('trackTime:', round(as.duration(now() - startTimeVar[[1]]), 2)))
+            }
         } else {
-            stop('Must call trackTime("start") before calling trackTime()')
+            stop(paste0('No timer with label "', label, '" found. Must call 
+                        trackTime("start") before calling trackTime()'))
         }
     } else {
         stop(paste('Unrecognized action "', action, '"', sep=''))
